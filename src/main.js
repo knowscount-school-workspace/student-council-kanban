813dcf62f55d0cc7165173401bccdfeb15a1076c
KnowsCount <knowscount@gmail.com>
2021-11-23 20:57:15 +0800

    feat: fix CORs issue (resolve #4)

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
-import Vue from "vue";
-import App from "./App.vue";
+import Vue from "vue"
+import App from "./App.vue"
+import axios from 'axios'
 
-Vue.config.productionTip = false;
+axios.defaults.withCredentials = true
+Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App)
-}).$mount("#app");
+}).$mount("#app")
